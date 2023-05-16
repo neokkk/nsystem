8c20f8090c781f129e210370f4b595892df4c9a1
neokkk <seven3126@gmail.com>
2023-05-16 21:37:36 +0900

    chore: 임시 커밋

--- main.c
+++ main.c
@@ -1,20 +1,47 @@
-#include <gui.h>
 #include <input.h>
+#include <gui.h>
 #include <system_server.h>
 #include <web_server.h>
+#include <signal.h>
 #include <sys/wait.h>
+
+void sigchld_handler(int sig)
+{
+  while (waitpid(-1, NULL, WNOHANG) > 0);
+}
 
 int main()
 {
-  pid_t i_pid = create_input();
-  pid_t ss_pid = create_system_server();
-  pid_t ws_pid = create_web_server();
-  pid_t g_pid = create_gui();
+  pid_t spid, gpid, ipid, wpid;
+  int status, saved_errno;
+  int sig_cnt;
+  sigset_t block_mask, empty_mask;
+  struct sigaction sa;
 
-  waitpid(i_pid, NULL, 0);
-  waitpid(ss_pid, NULL, 0);
-  waitpid(ws_pid, NULL, 0);
-  waitpid(g_pid, NULL, 0);
+  sa.sa_flags = 0;
+  sa.sa_handler = sigchld_handler;
+
+  sigemptyset(&sa.sa_mask);
+
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("sigaction error");
+    exit(-1);
+  }
+
+  printf("메인 함수입니다.\n");
+  printf("시스템 서버를 생성합니다.\n");
+  spid = create_system_server();
+  printf("웹 서버를 생성합니다.\n");
+  wpid = create_web_server();
+  printf("입력 프로세스를 생성합니다.\n");
+  ipid = create_input();
+  printf("GUI를 생성합니다.\n");
+  gpid = create_gui();
+
+  waitpid(ipid, NULL, 0);
+  waitpid(spid, NULL, 0);
+  waitpid(wpid, NULL, 0);
+  waitpid(gpid, NULL, 0);
 
   return 0;
 }
--- ui/input.h
+++ ui/input.h
@@ -1,11 +1,15 @@
 #ifndef _INPUT_H
 #define _INPUT_H
 
+#include <execinfo.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/prctl.h>
+#include <sys/signal.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <ucontext.h>
 
 int create_input();
 
