f622fda2d4b54b1f3756f8c1a4702e309a11d34c
neokkk <seven3126@gmail.com>
2023-08-27 02:18:25 +0900

    fix: dump 안 되던 문제 해결

--- system/dump_state.c
+++ system/dump_state.c
@@ -25,12 +25,12 @@ int dump_file(const char *title, const char *path)
 	if ((fp = fopen(path, "r")) == NULL) {
 		err = errno;
 		printf("------ %s (%s) ------\n", title, path);
-        printf("*** %s: %s\n", path, strerror(err));
+		printf("*** %s: %s\n", path, strerror(err));
 		printf("\n");
 		exit(-1);
 	}
 
-	if (strncmp(path, "/proc/", 6) != 0 || strncmp(path, "/sys/", 5) != 0) return -1;
+	if ((strncmp(path, "/proc/", 6) != 0) && (strncmp(path, "/sys/", 5) != 0)) return -1;
 	if (stat(path, &st) != 0) return -1;
 
 	mtime = st.st_mtime;
@@ -40,7 +40,6 @@ int dump_file(const char *title, const char *path)
 
 	while (1) {
 		nread = getline(&line, &len, fp);
-		printf("nread: %d\n", nread);
 		if (nread <= 0) break;
 		printf(line);
 	}
@@ -59,15 +58,15 @@ void dump_state()
 	strftime(date, sizeof(date), DATETIME_FORMAT, localtime(&now));
 
 	printf("========================================================\n");
-	printf("== dumpstate: %s\n", date);
+	printf("== dumpstate: %s ==\n", date);
 	printf("========================================================\n");
 	dump_file("KERNEL", "/proc/version");
 	dump_file("MEMORY INFO", "/proc/meminfo");
-	// dump_file("VIRTUAL MEMORY STATS", "/proc/vmstat");
-	// dump_file("SLAB INFO", "/proc/slabinfo");
+	dump_file("VIRTUAL MEMORY STATS", "/proc/vmstat");
+	dump_file("SLAB INFO", "/proc/slabinfo");
 	dump_file("ZONE INFO", "/proc/zoneinfo");
-	// dump_file("PAGE TYPE INFO", "/proc/pagetypeinfo");
-	// dump_file("BUDDY INFO", "/proc/buddyinfo");
+	dump_file("PAGE TYPE INFO", "/proc/pagetypeinfo");
+	dump_file("BUDDY INFO", "/proc/buddyinfo");
 	dump_file("NETWORK DEV INFO", "/proc/net/dev");
 	dump_file("NETWORK ROUTES", "/proc/net/route");
 	dump_file("NETWORK ROUTES", "/proc/net/ipv6_route");
